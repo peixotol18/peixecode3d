7f8d33c2e0955a71b42765ffa60e15a9e1a71faa
Joao Peixoto <107553795+peixotol18@users.noreply.github.com>
2024-05-17 09:31:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo = new Date("2024-05-10T00.00.00");
-
-contadores[0].textContent = tempoObjetivo;
 
 for (let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
@@ -15,3 +11,30 @@ for (let i=0; i<botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo4 = new Date("2024-05-20T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempos[0]);
+
+function calculaTempo(tempos[0]){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias+"dias "+horas+"horas "+minutos+"minutos "+segundos+"segundos";
+}
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
